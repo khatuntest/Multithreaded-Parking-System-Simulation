f5fe2e092afe7ca0207c2ae84a8dd39aaf5f4137
MariamMahmoud2003 <146382004+MariamMahmoud2003@users.noreply.github.com>
2024-12-04 13:44:13 -0800

    Update SJF.java

--- short_job_first/SJF.java
+++ short_job_first/SJF.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SJF {
-    private List<Process> processes;
+    private final List<Process> processes;
     private int current,complete_processes;
     private void print(){
         System.out.println("PID\tArrival\tBurst\tEnd\tTurnaround\tWaiting\tExecuted order");
@@ -14,6 +14,7 @@ public class SJF {
             System.out.println(p.pid + "\t" + p.arrival + "\t\t" + p.burst + "\t\t" +
                     p.end + "\t\t" + p.turnaround + "\t\t" + p.waiting+"\t\t"+ p.executed);
         }
+        System.out.println("________________________________________________________________________");
     }
     public SJF(){
         processes=new ArrayList<>();
@@ -26,7 +27,7 @@ public class SJF {
     public void context_switch(){
         processes.sort(Comparator.comparing(p->p.arrival));
         boolean[] finished=new boolean[processes.size()];
-        int suirved=1;
+        int served=1;
         while(complete_processes<processes.size()){
             List <Process> readyQueue=new ArrayList<>();
             for(Process p:processes)
@@ -36,16 +37,21 @@ public class SJF {
                 current++;
                 continue;
             }
-            Process current_process= Collections.min(readyQueue, Comparator.comparingInt(p -> p.remaining));
+            Process current_process= Collections.min(readyQueue, Comparator.comparingInt(p -> p.burst));
+            System.out.println("Process "+current_process.pid+" is executed after waiting "+ (current-current_process.arrival)+" time from the moment its arrived");
+
             current+=current_process.remaining;
             current_process.end=current;
-            current_process.executed=suirved;
+            current_process.executed=served;
             current_process.turnaround=current_process.end- current_process.arrival;
             current_process.waiting=current_process.turnaround-current_process.burst;
             finished[current_process.pid-1]=true;
             complete_processes++;
-            suirved++;
+            served++;
+            System.out.println("Process "+current_process.pid+" completed at "+current+" time");
+            System.out.println("________________________________________________________________________");
         }
+        processes.sort(Comparator.comparing(p->p.pid));
         print();
     }
 }
