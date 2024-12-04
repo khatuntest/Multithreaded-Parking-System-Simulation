3c365f53297c080da89dc3b4e748974c9144929f
MariamMahmoud2003 <146382004+MariamMahmoud2003@users.noreply.github.com>
2024-12-04 14:06:55 -0800

    Update main.java

--- short_job_first/main.java
+++ short_job_first/main.java
@@ -17,6 +17,19 @@ public class Main {
         sjfScheduler.insert_process(p3);
         sjfScheduler.insert_process(p4);
 
+        Process p5 = new Process(5, 0, 6); // Process 1 arrives at time 0 with burst 6
+        Process p6 = new Process(6, 2, 4); // Process 2 arrives at time 2 with burst 4
+        Process p7 = new Process(7, 4, 5); // Process 3 arrives at time 4 with burst 5
+        Process p8 = new Process(8, 6, 2); // Process 4 arrives at time 6 with burst 2
+        Process p9 = new Process(9, 8, 3); // Process 5 arrives at time 8 with burst 3
+
+        // Insert processes into the scheduler
+        sjfScheduler.insert_process(p5);
+        sjfScheduler.insert_process(p6);
+        sjfScheduler.insert_process(p7);
+        sjfScheduler.insert_process(p8);
+        sjfScheduler.insert_process(p9);
+
         // Run the SJF scheduling with context switch
         sjfScheduler.context_switch();
     }
